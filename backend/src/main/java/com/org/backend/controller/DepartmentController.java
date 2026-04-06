93a77b3b73dd416f7a70b5068a6253715c7e007a
Nithin Godugu <nithingodugu24@gmail.com>
2026-04-06 21:41:51 +0530

    feat: department controller, service & repo

---
+++ backend/src/main/java/com/org/backend/controller/DepartmentController.java
@@ -0,0 +1,46 @@
+package com.org.backend.controller;
+
+import com.org.backend.dto.DepartmentCreateRequestDto;
+import com.org.backend.dto.DepartmentDto;
+import com.org.backend.dto.DepartmentUpdateRequestDto;
+import com.org.backend.service.DepartmentService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/departments")
+public class DepartmentController {
+
+    private final DepartmentService departmentService;
+
+    @GetMapping
+    public List<DepartmentDto> getAllDepartments(){
+        return departmentService.getAllDepartments();
+    }
+
+    @PostMapping
+    public DepartmentDto createDepartment(
+            @Valid @RequestBody DepartmentCreateRequestDto request
+            ){
+        return departmentService.createDepartment(request);
+    }
+
+    @PutMapping("/{departmentId}")
+    public DepartmentDto updateDepartment(
+            @PathVariable Long departmentId,
+            @Valid @RequestBody DepartmentUpdateRequestDto request
+            ){
+        return departmentService.updateDepartment(departmentId, request);
+    }
+
+    @GetMapping("/{departmentId}")
+    public DepartmentDto getDepartment(
+            @PathVariable Long departmentId
+    ){
+        return departmentService.getDepartment(departmentId);
+    }
+}
