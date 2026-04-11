e32e482da5b8efd12066191cfabf3f9d1718639c
Nithin Godugu <nithingodugu24@gmail.com>
2026-04-11 19:58:42 +0530

    feat: positionCode to uniquely identify

--- backend/src/main/java/com/org/backend/dto/PositionDto.java
+++ backend/src/main/java/com/org/backend/dto/PositionDto.java
@@ -3,6 +3,7 @@ package com.org.backend.dto;
 public record PositionDto(
         Long id,
         String titile,
+        String positionCode,
         Long departmentId,
         Long parentPositionId
 ) {
