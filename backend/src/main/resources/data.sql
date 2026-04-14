3da8132c4c765bbb0b2aa0793afd89e6a48639ab
rumanajaha <rumanajaha1@gmail.com>
2026-04-14 22:51:22 +0530

    fix/data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -15,4 +15,4 @@ INSERT INTO positions (id, title, department_id, position_code) VALUES
 
 -- EMPLOYEES
 INSERT INTO employees (id, employee_code, first_name, email, position_id) VALUES
-(1, 'ADM001', 'Test', 'admin@gmail.com', 1);                                                       (2, 'manager',  '$2a$10$uPCqwmn9EC56y2IynHROXOhEoXFUlgt0krCp7l9YJmi9GwaNYTyi                                                                                 (3, 'employee', '$2a$10$uPCqwmn9EC56y2IynHROXOhEoXFUlgt0krCp7l9YJmi9GwaNYTyiK', 3, 'ACTIVE', 'EMPLOYEE');
+(1, 'ADM001', 'Test', 'admin@gmail.com', 1);
